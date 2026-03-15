a85738f85d9fe4e8245500a3c9c5f137c071efea
Rob Garrison <wowmotty@gmail.com>
2026-03-15 06:58:22 -0500

    Vite 8 & no quik

--- README.md
+++ README.md
@@ -1,28 +1,10 @@
-# Qwik + Vite
-
-## Qwik in CSR mode
-
-This starter is using a pure CSR (Client-Side Rendering) mode. This means, that the application is fully bootstrapped in the browser. Most of Qwik innovations however take advantage of SSR (Server-Side Rendering) mode.
-
-```ts
-export default defineConfig({
-  plugins: [
-    qwikVite({
-      csr: true,
-    }),
-  ],
-})
-```
-
-Use `npm create qwik@latest` to create a full production ready Qwik application, using SSR and [QwikCity](https://qwik.dev/docs/qwikcity/), our server-side metaframwork.
+# Vite
 
 ## Usage
 
 ```bash
 $ npm install # or pnpm install or yarn install
 ```
-
-Learn more on the [Qwik Website](https://qwik.dev) and join our community on our [Discord](https://qwik.dev/chat)
 
 ## Available Scripts
 
--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,26 @@
-import { component$, useSignal } from '@builder.io/qwik'
+import { useState } from 'react';
+import viteLogo from '/vite.svg';
+import './App.css';
 
-import qwikLogo from './assets/qwik.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
-
-export const App = component$(() => {
-  const count = useSignal(0)
+const App = () => {
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://qwik.dev" target="_blank">
-          <img src={qwikLogo} class="logo qwik" alt="Qwik logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + Qwik</h1>
-      <div class="card">
-        <button onClick$={() => count.value++}>count is {count.value}</button>
+      <h1>Vite</h1>
+      <div className="card">
+        <button onClick={() => setCount(count + 1)}>count is {count}</button>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Qwik logos to learn more
+      <p className="read-the-docs">
+        Click on the Vite logo to learn more
       </p>
     </>
   )
-})
+};
+
+export default App;
